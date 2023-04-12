399271a45dfbff6d8f461d2e157dd72b44af4c59
Yuda <pahalayjs@gmail.com>
2023-04-12 09:40:20 +0700

    Commit newtransactionview UI & contentview (detil transaction) ui

---
+++ SettleDebtView.swift
@@ -0,0 +1,20 @@
+//
+//  SettleDebtView.swift
+//  TuruDebt
+//
+//  Created by Pahala Sihombing on 12/04/23.
+//
+
+import SwiftUI
+
+struct SettleDebtView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettleDebtView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettleDebtView()
+    }
+}
--- TuruDebt/NewTransactionView.swift
+++ TuruDebt/NewTransactionView.swift
@@ -7,94 +7,144 @@
 
 import SwiftUI
 
-
-
 struct NewTransactionView: View {
     @State private var name: String = ""
     @State private var nominal: String = ""
     @State private var note: String = ""
+    @State private var utang = false
+    
+    let defaults = UserDefaults.standard
+    
+    
+    func saveNama(name: String){
+        defaults.set(name, forKey: "Nama")
+        
+    }
+    
     
     enum Status: String, CaseIterable, Identifiable {
-        case youOweMe, iOweYou
+        case iOweYou, youOweMe
         var id: Self { self }
     }
     
-    @State private var selectedStatus: Status = .youOweMe
+    @State private var selectedStatus: Status = .iOweYou
+    
+    let gradientColors = [
+        Color(hex: 0xFF7090),
+        Color(hex: 0x8FCBFF)
+    ]
     
     var body: some View {
         VStack {
             VStack{
                 VStack{
-                    Text("Swipe down to close")
-                    Image(systemName: "chevron.compact.down")
-                        .fontWeight(.bold)
-                        .font(.largeTitle)
+                    Text("Slide to back to Home")
+                        .padding(.bottom,6)
+                    Image(systemName: "chevron.down")
                 }
+                .padding(.top,25)
+                .padding(.bottom,25)
+                HStack {
+                    Text("Fill")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor( selectedStatus == Status.iOweYou ? Color(hex: 0xFF7090) : Color(hex: 0x8FCBFF))
+                    Text("the Form")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                }
+                .padding(.bottom, 30)
                 HStack{
                     Text("Name")
+                        .foregroundColor(.gray)
                     Spacer()
                 }
                 TextField(
                     "Write your name",
                     text: $name
                 )
-                .frame(height: 48)
+                
+                .frame(height: 41)
                 .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
-                .cornerRadius(5)
-                .background(Color(hex: 0xD9D9D9, alpha: 1))
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 4, x: 2, y: 2)
+                .padding(.bottom, 24.0)
                 
                 HStack{
                     Text("Nominal")
+                        .foregroundColor(.gray)
                     Spacer()
                 }
                 TextField(
                     "Nominal transaction",
                     text: $nominal
                 )
-                .frame(height: 48)
+                .frame(height: 41)
                 .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
-                .cornerRadius(5)
-                .background(Color(hex: 0xD9D9D9, alpha: 1))
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 4, x: 2, y: 2)
+                
+                .padding(.bottom, 24.0)
                 
                 HStack{
                     Text("Note")
+                        .foregroundColor(.gray)
                     Spacer()
                 }
                 TextField(
                     "Reason",
                     text: $note
                 )
-                .frame(height: 48)
+                .frame(height: 41)
                 .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
-                .cornerRadius(5)
-                .background(Color(hex: 0xD9D9D9, alpha: 1))
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 4, x: 2, y: 2)
+                .padding(.bottom, 24.0)
                 
             }
             .padding(.horizontal)
             
-            
-            Picker("Status", selection: $selectedStatus) {
-                Text("You Owe Me \(name)")
-                Text("I Owe You \(name)")
+            VStack{
+                HStack{
+                    Text("What are you?")
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                HStack{
+                    Picker("Status", selection: $selectedStatus) {
+                        Text("I Owe You \(name)").tag(Status.iOweYou)
+                        Text("You Owe Me \(name)").tag(Status.youOweMe)                       
+                    }
+                    .accentColor(selectedStatus == Status.iOweYou ? Color(hex: 0xFF7090) : Color(hex: 0x8FCBFF))
+                    Spacer()
+                }
+                
+                Spacer()
             }
             
-            Spacer()
-            
+            .padding()
+            Text(defaults.string(forKey: "Nama") ?? "")
+            Spacer(minLength:10)
             Button(action: {
                 print("Clicked")
+                saveNama(name:name)
             }, label: {
                 HStack {
-                   
+                    
                     Image(systemName: "paperplane.fill")
                     
                     Text("Save")
                 }
             })
             .foregroundColor(.white)
+            .background(selectedStatus == Status.iOweYou ? Color(hex: 0xFF7090) : Color(hex: 0x8FCBFF))
             .cornerRadius(5)
-            .frame(height:55)
+            .frame(height:25)
             .padding(0)
-            .buttonStyle(.borderedProminent)
+            .buttonStyle(.bordered)
         }
     }
 }
